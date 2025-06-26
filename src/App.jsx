914bbb5d4ea1419172809252938cde5e298f7cbe
Aniket25-11 <Www.aniketbhojapure@gmail.com>
2025-06-27 01:12:02 +0530

    Navbar - Component done

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Navbar from './components/Navbar';
+
+
+function App() {
+  return (
+    <>
+      <section>
+        <Navbar />
+
+      </section>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import styles from './Navbar.module.scss';
+import bg from '../images/Navbar-bg-image.png';
+
+function Navbar() {
+    return (
+        <div>
+            <div className={styles['bg-main']} style={{ backgroundImage: `url(${bg})` }} >
+                <section className={styles['bg-container']} >
+                    <h3>Luminous Labs</h3>
+                    <div className={styles['NavTabs']}>
+                        <p>Technology</p>
+                        <p>Company</p>
+                        <p>Commercial</p>
+                        <p>Blog</p>
+                        <div className={styles['shop']}>
+                            <p>Shop</p>
+                        </div>
+                    </div>
+                    <div className={styles['cartIcon']}>
+                        <h4>EN <span><i class="ri-arrow-down-s-line"></i></span></h4>
+                        <h4>Cart <span className={styles['bagline']}><i class="ri-shopping-bag-line"></i></span></h4>
+                    </div>
+                </section>
+                <section2>
+                    <p className={styles['para']}>Red Light Therapy: Proven, safe, and non-invasive</p>
+                    <h1 className={styles['head']}>Your Cells, Supercharged</h1>
+                </section2>
+            </div>
+        </div>
+    );
+}
+
+export default Navbar;
